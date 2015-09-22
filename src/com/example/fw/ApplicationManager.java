9eded61e7c67f73348f0c26b4fb046ac9ec6f77f
Elena.Borodina <Elena.Borodina@billing.ru>
2015-09-22 18:45:33 +0300

    запуск из консоли + скрипт

--- src/com/example/fw/ApplicationManager.java
+++ src/com/example/fw/ApplicationManager.java
@@ -1,8 +1,10 @@
 package com.example.fw;
 
-import java.util.concurrent.TimeUnit;
+import java.net.URL;
+import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class ApplicationManager { 
 	public WebDriver driver;
@@ -12,12 +14,23 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+	private Properties properties;
 	
-	public ApplicationManager(){
-		 driver = new FirefoxDriver();
-		 baseUrl = "https://localhost/";
-		 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		 driver.get(baseUrl + "addressbookv4.1.4/");
+	public ApplicationManager(Properties properties){
+		 this.properties = properties;
+		 String browser = properties.getProperty("browser");
+		 if ("firefox".equals(browser)) {
+			 driver = new FirefoxDriver();
+		 } else if ("ie".equals(browser)) {
+			 driver = new InternetExplorerDriver() ;
+			 } else {
+				 throw new Error ("Unsupported browser: " + browser);
+			 }
+		 
+		
+		 baseUrl = properties.getProperty("baseUrl");
+	//	 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		 driver.get(baseUrl);
 				 
 		 //1-й способ - ѕредварительна€ инициализаци€ 
 		// navigationHelper = new NavigationHelper(this); 
---
+++ test.bat
@@ -0,0 +1,2 @@
+cd c:\test\test_pdt-25
+java -cp bin;C:\test\JavaLib\xstream-1.4.4.jar;C:\test\JavaLib\xpp3-1.1.4.jar;C:\test\JavaLib\xmlpull-1.1.3.1.jar;c:\test\JavaLib\selenium-server-standalone-2.47.1.jar org.testng.TestNG testng-customsuite.xml
